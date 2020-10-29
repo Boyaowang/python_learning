1b4d40555943a60150a2c61302a5ed990f2dba85
Boyao Wang <wangboyao17@outlook.com>
2020-10-30 00:38:58 +0100

    my first python program

---
+++ axin.py
@@ -0,0 +1,46 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__mai__':
+    print_hi('PyCharm')
+elif __name__ == '__man__':
+    print_hi('PyCharm2')
+elif __name__ == '__main__':
+    print_hi('PyCharm3')
+elif __name__ == '__mai__':
+    print_hi('PyCharm4')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+name = ['axin', 'zixin', 'xiaomaomao','sjdhfijsh', 'ashdifvcuhwapoiusrveh', 'ashdivuhasidhv']
+
+for pname in name[1:5]:
+    print(pname)
+
+for x in range(1,100,10):
+    print(x)
+
+'''
+a = 10
+x = 0
+while x< a:
+    x += 1
+    print(x)
+'''
+
+print(7, ' cat')
+print(9, ' xiaomei')
+
+
+for n in range(100):
+    if(n%4==0):
+        print(n)
+
